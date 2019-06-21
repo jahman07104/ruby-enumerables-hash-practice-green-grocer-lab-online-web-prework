519c04dbeed9568e08f775b155e2f372d4908fda
Patrick Harrison <jahman@bellatlantic.net>
2019-06-21 15:11:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,5 +38,15 @@ def checkout(cart, coupons)
 		{"CHEESE" => {:price => 6.50, :clearance => false}},
 		{"BEER" => {:price => 13.00, :clearance => false}},
 		{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
-		 # code here
+		 
+		{:item => "AVOCADO", :num => 2, :cost => 5.00},
+		{:item => "BEER", :num => 2, :cost => 20.00},
+		{:item => "CHEESE", :num => 3, :cost => 15.00}
+	def apply_clearance(cart)
+  # def generate_cart
+	[].tap do |cart|
+		rand(20).times do
+			cart.push(items.sample)
+  		end }
 end
+}
